cd1e8abb6ffdd04c8c90adf8e1e0cde60e9fb5d2
welson <wlul.lin@gmail.com>
2015-03-31 15:04:29 +0800

    remove un-used file

--- data/service/backup_restore/counter/dump_counter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT="8080"
-
-echo "Dump BrowsingCounter ..."
-curl localhost:${PORT}/beagle/browsingCount?action=dump\&threshold=3
-
-echo "Dump CuttingCounter ..."
-curl localhost:${PORT}/beagle/cuttingCount?action=dump\&threshold=3
