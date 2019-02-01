ff09849345260f5f8159b7f4fe26337a667c8f72
welson <welsonlin@pchome.tw>
2019-02-01 11:51:04 +0800

    update script

--- build.sh
+++ build.sh
@@ -1,37 +1,63 @@
 #!/bin/bash
 
 source "change_version.sh"
+source "data/path.sh"
 
 echo "Old version = $(get_current_version)"
 
-
-tmpfolder=$(mktemp -d /tmp/tmp.XXXXXXXX)
 ver="$(promote_version)"
+tmpfolder=$(mktemp -d /tmp/tmp.XXXXXXXX)
 tmpbin="${tmpfolder}/install.bin"
-bin="output_bin/wels_installer-${ver}.bin"
+tmpdata="${tmpfolder}/data"
+tmp_rails_home="${tmpdata}/ws/rails"
+bin="output_bin/myapp_installer-${ver}.bin"
 
-
-PACK_DATA_HOME="/Users/welson/rails/wels"
+RAILS_REPO_HOME="$1"
 TAR="servicedata.tar.gz"
 
-#~ cp -rf data ${tmpfolder}
-#~ cp -rf init_server.sh ${tmpfolder}
-#~ #cp -rf extract.sh $tmpbin
-if [ ! -d "${PACK_DATA_HOME}" ]; then
-	echo "${PACK_DATA_HOME} is not found!"
+prepare_data() {
+	mkdir -p ${tmpdata}
+	mkdir -p output_bin
+
+	echo "cp -rf data/* ${tmpdata}"
+	cp -rf data/* ${tmpdata}
+
+	# drop softlink
+	echo "Remove softlink:"
+	echo "rm -rf ${tmp_rails_home}/public/sitemap"
+	rm -rf ${tmp_rails_home}/public/sitemap
+	echo "rm -rf ${tmp_rails_home}/public/temp"
+	rm -rf ${tmp_rails_home}/public/temp
+
+	# drop log,tmp file
+	echo "Drop ${tmp_rails_home}/log/*"
+	rm -rf ${tmp_rails_home}/log/*
+	echo "Drop ${tmp_rails_home}/tmp/*"
+	rm -rf ${tmp_rails_home}/tmp/*
+}
+
+
+if [ ! -d "${RAILS_REPO_HOME}" ]; then
+	# RAILS_REPO_HOME="data/ws/rails"
+	echo "Argument 1, ws_folder_path, is not found!"
 	exit
 else
-	mkdir -p data/rails
-	rm -rf data/rails/*
-	cp -rf ${PACK_DATA_HOME}/* data/rails
+	prepare_data
 fi
 
-#tar --exclude-vcs -czf $TAR data windows
-tar -czf $TAR data
+
+if [[ "$OSTYPE" == *win* ]]; then 
+	# mac, unix
+	tar --exclude=.svn --exclude=.git -czf $TAR -C ${tmpfolder} .
+else
+	# linux
+	tar --exclude-vcs -czf $TAR -C ${tmpfolder} .
+fi
 cat init_server.sh $TAR >> $tmpbin
 chmod +x $tmpbin
-cp -f $tmpbin $bin
+mv -f $tmpbin $bin
 
+echo "rm -rf ${tmpfolder} ..."
 rm -rf ${tmpfolder}
 
 
